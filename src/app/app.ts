fcc8c849c00c8bcb2c8d6b92353e962c142d6696
joelvitorniino <joelprofessor13@gmail.com>
2022-11-17 10:38:46 -0300

    add route /auth/google

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import { router } from './routes';
+import { GoogleoAuth2Service } from './services/GoogleoAuth2Service';
 
 const app = express();
+const googleService = new GoogleoAuth2Service();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(router);
+
+googleService.serializeUser();
+googleService.deserializeUser();
 
 app.listen(3000, () => {
     console.log("Running on port 3000")
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,10 +1,13 @@
 import { Request, Response, Router } from "express";
 import MainController from "./controllers/MainController";
 import ProtectedController from "./controllers/ProtectedController";
+import { GoogleoAuth2Service } from "./services/GoogleoAuth2Service";
 
 const router = Router();
+const passport = new GoogleoAuth2Service().createPassport();
 
 router.get('/', MainController.index);
+router.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
 router.get('/protected', ProtectedController.index);
 
 export { router };
