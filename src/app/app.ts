0773637e275a304729bd7a332b36f4fb7977d8f8
joelvitorniino <joelprofessor13@gmail.com>
2022-11-17 09:50:00 -0300

    include routes on app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import { router } from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
     console.log("Running on port 3000")
