6fda136876f43a910b4dceb4b0a5c703091938c3
joelvitorniino <joelprofessor13@gmail.com>
2022-11-17 09:45:23 -0300

    l
    include dotenv

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,1 +1,10 @@
+import express from 'express';
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+    console.log("Running on port 3000")
+});
